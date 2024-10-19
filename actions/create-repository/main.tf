c1d2bf08939d1d6e26bc420b63e2cc9583c88d4b
Adelino Sousa <lino.sousa.86@gmail.com>
2024-10-19 22:38:30 +0100

    Removed branch protection from private branch

--- actions/create-repository/main.tf
+++ actions/create-repository/main.tf
@@ -77,25 +77,26 @@ resource "github_branch_default" "default" {
 }
 
 resource "github_branch_protection" "default" {
+  count                           = var.repository_private || var.github_organisation == "" ? 0 : 1
   repository_id                   = github_repository.repository.id
   pattern                         = github_branch_default.default.branch
-  # enforce_admins                  = true
-  # require_signed_commits          = true
-  # required_linear_history         = true
-  # require_conversation_resolution = true
+  enforce_admins                  = true
+  require_signed_commits          = true
+  required_linear_history         = true
+  require_conversation_resolution = true
 
-  # required_pull_request_reviews {
-  #   dismiss_stale_reviews           = true
-  #   restrict_dismissals             = var.repository_private || var.github_organisation == "" ? false : true
-  #   require_code_owner_reviews      = var.repository_private || var.github_organisation == "" ? false : true
-  #   required_approving_review_count = 1
-  #   require_last_push_approval      = true
-  # }
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    restrict_dismissals             = true
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+    require_last_push_approval      = true
+  }
 
-  # required_status_checks {
-  #   strict   = true
-  #   contexts = []
-  # }
+  required_status_checks {
+    strict   = true
+    contexts = []
+  }
 
   lifecycle {
     ignore_changes = [
